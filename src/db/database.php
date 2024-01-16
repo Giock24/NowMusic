79cccec394658241d20e174f0a84835a05767d21
Fabrizio Marzo <fabrizio.marzo@studio.unibo.it>
2024-01-16 14:53:32 +0100

    [ Database ] Aggiuta capmpo port su new mysqli

--- src/db/database.php
+++ src/db/database.php
@@ -4,8 +4,8 @@ class DatabaseHelper {
 
     private $db;
 
-    public function __construct($servername, $username, $password, $dbname) {
-        $this->db = new mysqli($servername, $username, $password, $dbname);
+    public function __construct($servername, $username, $password, $dbname,$port) {
+        $this->db = new mysqli($servername, $username, $password, $dbname,$port);
 
         if ($this->db->connect_error) {
             die("Connection Failed: " . $db->connect_error);
