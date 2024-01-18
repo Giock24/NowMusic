e75e602d7b796e0f7279dc083428d3973d1e8c5b
riccardo gardenghi <richy.gardenghi@gmail.com>
2024-01-18 20:14:59 +0100

    - [Profile] - fix change image button color

--- src/profile/profile_page.php
+++ src/profile/profile_page.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     <link rel="stylesheet" href="./profile_page.css">
     <link rel="stylesheet" href="../../css/style.css"/>
+    <link rel="stylesheet" href="../../css/form.css"/>
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
