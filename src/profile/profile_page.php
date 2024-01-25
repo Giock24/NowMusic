142eeac66c1e42de88ebcf4efb26c065d93ac035
Riccardo Gardenghi <riccardo.gardenghi@studio.unibo.it>
2024-01-25 10:45:39 +0100

    - [Profile] - fix accessibility

--- src/profile/profile_page.php
+++ src/profile/profile_page.php
@@ -12,6 +12,7 @@
     <link rel="stylesheet" href="../../css/style.css"/>
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="./profile_page_style.css">
 </head>
 <body>
     <?php
@@ -22,7 +23,7 @@
                     alert("Fail download Image !");
                 </script>
     <?php endif; ?>
-    <div class="container-fluid overflow-x-hidden p-0 h-100">
+    <div class="profilePageMain container-fluid overflow-x-hidden p-0 h-100">
         <div class="d-flex flex-column h-100">
             <div  class="flex-grow-0">
                 <?php 
